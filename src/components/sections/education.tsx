a76933f14e049fecef261b8a1b351624ae3a3341
KKapkoti <kavitakapkoti.kk123@gmail.com>
2025-06-27 17:25:57 +0530

    first commit

--- src/components/sections/education.tsx
+++ src/components/sections/education.tsx
@@ -6,7 +6,7 @@ import Container from '@/components/layout/container';
 
 const EducationSection = () => {
   return (
-    <Container id="Education" className="bg-gray-50">
+    <Container id="education" className="bg-gray-50">
       <div className="flex flex-col items-center gap-4">
         <div className="self-center">
           <Tag label="Education" />
--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -6,7 +6,7 @@ import Container from '@/components/layout/container';
 
 const SkillsSection = () => {
   return (
-    <Container>
+    <Container id="skills" >
       <div className="flex flex-col items-center gap-4">
         <div className="self-center">
           <Tag label="Skills" />
@@ -17,7 +17,7 @@ const SkillsSection = () => {
       </div>
 
       <div className="grid grid-cols-3 gap-y-4 md:grid-cols-6 md:gap-y-8 lg:grid-cols-8 lg:gap-y-12">
-        {TECHNOLOGIES.map((technology, index) => (
+        {TECHNOLOGIES?.map((technology, index) => (
           <TechDetails {...technology} key={index} />
         ))}
       </div>
